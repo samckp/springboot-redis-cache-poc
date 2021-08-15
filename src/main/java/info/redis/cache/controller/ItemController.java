830dc01028bfeecf3e86bf018bf7f0a7af38c93a
samckp <sam.mca.liet@gmail.com>
2021-08-15 07:25:58 +0530

    updated

--- src/main/java/info/redis/cache/controller/ItemController.java
+++ src/main/java/info/redis/cache/controller/ItemController.java
@@ -3,7 +3,12 @@ package info.redis.cache.controller;
 import info.redis.cache.dao.Item;
 import info.redis.cache.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/item")
@@ -12,20 +17,34 @@ public class ItemController {
     @Autowired
     private ItemService itemService ;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Item>> getAllItem() {
+
+        return new ResponseEntity<>(itemService.items(), HttpStatus.OK);
+    }
     @GetMapping("/{id}")
-    public Item getItem(@PathVariable Integer id) {
-        return itemService.getItem(id);
+    public ResponseEntity getItem(@PathVariable Integer id) {
+
+        return new ResponseEntity(itemService.getItem(id), HttpStatus.OK);
     }
 
-    @PostMapping("/")
-    public Item createItem(@RequestBody Item item){
+    @PostMapping("/create")
+    public ResponseEntity createItem(@RequestBody Item item){
 
-        return itemService.createItem(item);
+        return new ResponseEntity<>(itemService.createItem(item), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public Item updateItem(@PathVariable Integer id, @RequestBody Item item){
+    public ResponseEntity updateItem(@PathVariable Integer id, @RequestBody Item item){
 
-        return itemService.updateItem(id, item);
+        return new ResponseEntity<>(itemService.updateItem(id, item), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteItem(@PathVariable Integer id){
+
+         //return new ResponseEntity<>(itemService.deleteItem(id), HttpStatus.OK);
+
+          itemService.deleteItem(id);
     }
 }
